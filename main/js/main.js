be929200f0b1fa61f590f65fae5e075fdadd78ea
Rammina <altinaschwarzer@gmail.com>
2019-04-18 20:46:29 +0800

    added menu items, description for perks

--- main/js/main.js
+++ main/js/main.js
@@ -1,1 +1,29 @@
-// kjhkljh
+// document objects
+
+// navigation bar objects
+
+let navbar = {
+	header: document.querySelector(".main-header"),
+	title: document.querySelector("#nav__title"),
+	menubutton: document.querySelector("#nav__menu"),
+	menuitems: document.querySelector(".nav__items")
+
+}
+
+navbar.menubutton.addEventListener("click", function showMenu(){
+	navbar.menuitems.classList.toggle("show");
+});
+
+window.onscroll = function (event)
+{
+	if(this.scrollY > 100) {
+		navbar.header.classList.add("show");
+		navbar.title.classList.add("show");
+
+	}
+	else if(this.scrollY < 100) {
+		navbar.header.classList.remove("show");
+		navbar.title.classList.remove("show");
+
+	}
+}
