7fbf61a9914e5d6ab05e5f32ac78ba9acbf45d71
Rammina <altinaschwarzer@gmail.com>
2019-04-24 10:32:57 +0800

    open backdrop upon clicking product overlay

--- main/js/main.js
+++ main/js/main.js
@@ -10,6 +10,19 @@ let navbar = {
 
 }
 
+// Main objects
+
+// Product section object
+let products = { 
+	items: document.querySelectorAll(".products__overlay")
+};
+
+// Backdrops and pop-ups
+let popup = {
+	backdrop: document.querySelector(".backdrop"),
+	close: document.querySelector(".popup__close")
+	};
+
 
 
 navbar.menubutton.addEventListener("click", function showMenu(){
@@ -17,16 +30,29 @@ navbar.menubutton.addEventListener("click", function showMenu(){
 });
 
 
-
+// This checks how much the window is scrolled
 window.onscroll = function (event)
 {
+	// Makes the navbar opaque After scrolling down 100 pixels
 	if(this.scrollY > 100 ) {
 		navbar.header.classList.add("show");
 		navbar.title.classList.add("show");
 		
 	}
+	// Makes the navbar transparent After scrolling up past 100 pixels of the window
 	else if(this.scrollY < 100) {
 		navbar.header.classList.remove("show");
 		navbar.title.classList.remove("show");
 	}
 }
+
+// Clicking each item in the products will open the backdrop pop-up window
+for (let item of products.items){
+	item.addEventListener("click", function(){
+		popup.backdrop.classList.add("show");
+	});
+}
+
+popup.close.addEventListener("click", function(){
+	popup.backdrop.classList.remove("show");
+});
