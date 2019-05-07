5957fb4569cd12aa29d8f83838108b3655447244
Rammina <altinaschwarzer@gmail.com>
2019-05-07 20:25:02 +0800

    finished breakpoints for footer

--- main/js/main.js
+++ main/js/main.js
@@ -19,6 +19,8 @@ var navbar = {
 
 // Main objects
 
+
+// Services section
 var services = { 
 	title: document.querySelector("#services")
 
@@ -28,6 +30,29 @@ var products = {
 	title: document.querySelector("#products"),
 	items: document.querySelectorAll(".products__overlay")
 };
+
+// about section
+var about = {
+	title: document.querySelector("#about")
+}
+
+// founders section
+var founders = {
+	title: document.querySelector("#founders"),
+	svgs: document.querySelectorAll(".founders__svg"),
+	links: document.querySelectorAll(".founders__logo-link")
+}
+
+// contact section
+var contact = {
+	container: document.querySelector(".contact-container"),
+	emailInput: document.querySelector(".contact__text-input"),
+	emailInputText: document.querySelector(".contact__text-input").value,
+	emailClick: false,
+	signUp: document.querySelector("#contact-submit"),
+	signUpValid: false
+}
+
 
 // Backdrops and pop-ups
 var popup = {
@@ -47,22 +72,7 @@ var popup = {
 	// tools: document.querySelector("#popup-tools")
 	};
 
-var founders = {
 
-	svgs: document.querySelectorAll(".founders__svg"),
-	links: document.querySelectorAll(".founders__logo-link")
-}
-
-// contact objects
-
-var contact = {
-
-	emailInput: document.querySelector(".contact__text-input"),
-	emailInputText: document.querySelector(".contact__text-input").value,
-	emailClick: false,
-	signUp: document.querySelector("#contact-submit"),
-	signUpValid: false
-}
 
 navbar.menubutton.addEventListener("touchstart", function () {
 
@@ -93,15 +103,20 @@ navbar.menubutton.addEventListener("click", function () {
 
 
 // Get the offset top of the section elements 
-var servicesY = services.title.offsetTop - 48;
-var productsY = products.title.offsetTop - 48;
+var servicesY = services.title.offsetTop - 60;
+var productsY = products.title.offsetTop - 60;
+var aboutY = about.title.offsetTop - 60;
+var foundersY = founders.title.offsetTop - 60;
+var contactY = contact.container.offsetTop - 120;
 
 // Re-updates the offset upon resizing
 window.onresize = function (event) {
 
-	servicesY = services.title.offsetTop - 48;
-	productsY = products.title.offsetTop - 48;
-
+	servicesY = services.title.offsetTop - 60;
+	productsY = products.title.offsetTop - 60;
+	aboutY = about.title.offsetTop - 60;
+	foundersY = founders.title.offsetTop - 60;
+	contactY = contact.container.offsetTop - 120;
 	// console.log(servicesY);
 };
 
@@ -146,16 +161,46 @@ window.onscroll = function (event)
 
 	}
 	// Highlight products after scrolling down to its position
-	else if(this.scrollY > productsY) {
+	else if(this.scrollY > productsY && this.scrollY < aboutY) {
 		for(let index = 0; index < navbar.menuitem.length; index++) {
-			if(index !== 1) {
-				navbar.menuitem[index].classList.remove("current");
+			if(index !== 1){
+
+			 navbar.menuitem[index].classList.remove("current");
 
 			}
-
 		}
-		navbar.menuitem[1].classList.add("current");
+	navbar.menuitem[1].classList.add("current");
+	}
 
+	// Highlight about after scrolling down to its position
+	else if(this.scrollY > aboutY && this.scrollY < foundersY) {
+		for(let index = 0; index < navbar.menuitem.length; index++) {
+			if(index !== 2) {
+				navbar.menuitem[index].classList.remove("current");
+			}
+		}
+		navbar.menuitem[2].classList.add("current");
+	}
+
+	// Highlight founders after scrolling down to its position
+	else if(this.scrollY > foundersY && this.scrollY < contactY) {
+		for(let index = 0; index < navbar.menuitem.length; index++) {
+			if(index !== 3) {
+				navbar.menuitem[index].classList.remove("current");
+			}
+		}
+		navbar.menuitem[3].classList.add("current");
+	}
+
+	// Highlight contact after scrolling down to its position
+	else if(this.scrollY > contactY) {
+		for(let index = 0; index < navbar.menuitem.length; index++) {
+			if(index !== 4){
+
+			 	navbar.menuitem[index].classList.remove("current");
+			}
+		}
+		navbar.menuitem[4].classList.add("current");
 	}
 
 }
